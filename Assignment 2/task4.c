09ce0d564fbf553a6e610ae077995ce5968263a7
Harshit Pandit <harshit7757@gmail.com>
2023-10-29 15:10:59 +0000

    Sorting but fails for quotes

--- Assignment 2/task4.c
+++ Assignment 2/task4.c
@@ -25,6 +25,8 @@ void extractBufferData(char *buffer){
 	GameInfo.score=0;
 	GameInfo.releaseYear=0;
 
+	// whenever we are encountered with quotes, it still follows the ignoration logic should not be happening
+
 	for(int i=0;i<strlen(buffer);i++){
 		c=buffer[i];
 		if(c==',' || c=='\n'){
@@ -81,6 +83,63 @@ void copyStructGameData(game *dest,game *src){
 	dest->releaseYear=src->releaseYear;
 }
 
+// here we sort the games as per there score
+
+void merge(game arr[],int left,int mid,int right){
+	int n1=mid-left+1,n2=right-mid;
+
+	game arr1[n1],arr2[n2]; // array of structs
+	
+	int i=0,j=0;
+	// filling up the separated arrays
+	for(;i<n1;i++){
+		copyStructGameData(&arr1[i],&arr[left+i]);
+		// arr1[i]=arr[left+i];
+	}
+	for(;j<n2;j++){
+		copyStructGameData(&arr2[j],&arr[mid+j+1]);
+		// arr2[j]=arr[mid+j+1];
+	}
+
+	i=0,j=0;
+	int k=left;
+	// now adding them back the ascending order
+	while(i!=n1 && j!=n2){
+		if(arr1[i].score>arr2[j].score){
+			copyStructGameData(&arr[k],&arr1[i]);
+			// arr[k]=arr1[i];
+			i++;
+		}
+		else{
+			copyStructGameData(&arr[k],&arr2[j]);
+			// arr[k]=arr2[j];
+			j++;
+		}
+		k++;
+	}
+	
+	// add the remaining elements if are any left
+	while(i<n1){
+		copyStructGameData(&arr[k],&arr1[i]);
+		// arr[k]=arr1[i];
+		i++,k++;
+	}
+	while(j<n2){
+		copyStructGameData(&arr[k],&arr2[j]);
+		// arr[k]=arr2[j];
+		j++,k++;
+	}
+}
+
+void mergeSort(game arr[],int left,int right){
+	if(left<right){
+		int mid = (left+right)/2;
+		mergeSort(arr,left,mid);
+		mergeSort(arr,mid+1,right);
+
+		merge(arr,left,mid,right);
+	}
+}
 
 int main(int argc,char *argv[]){
 	FILE *csv;
@@ -94,10 +153,10 @@ int main(int argc,char *argv[]){
 	fileParser(csv);
 
 	// taking arrayIndex because that is the actual size of the array
-	//mergeSortScore(GameArray,0,ARRAY_SIZE-1);
+	mergeSort(GameArray,0,arrayIndex-1);
 
 	game temp;
-	
+
 	for(int i=0;i<arrayIndex;i++){
 		temp=GameArray[i];
 		printf("Title:%s\n",temp.title);
